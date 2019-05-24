6fb09df1771eccf00f640aedea034df9a63c1467
Tom's <toms@Tomss-MacBook-Pro.local>
2019-05-25 02:03:43 +0800

    add route perusahaan update

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,7 @@ Route::get('/admin','adminController@index')->name('admin_index');
 //perusahaan
 Route::get('/perusahaan','adminController@perusahaan_index')->name('perusahaan_index');
 Route::get('/perusahaan/detail/{id}','adminController@perusahaan_detail')->name('perusahaan_detail');
+Route::put('/perusahaan/detail/{id}','adminController@perusahaan_update')->name('perusahaan_update');
 
 
 //Retribusi Kalibrasi
