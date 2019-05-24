ca5f1f652dc00f6b29667b3575f464a08a8fbc36
Tom's <toms@Tomss-MacBook-Pro.local>
2019-05-24 23:25:04 +0800

    make migration perusahaan

---
+++ database/migrations/2019_05_24_151359_create_perusahans_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePerusahansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('perusahans', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('id_user');
+            $table->string('nama')->length('255');
+            $table->string('alamat')->length('255');
+            $table->string('telepon')->length('13');
+            $table->tinyInteger('status')->default(0);
+            $table->string('gambar')->length('255')->nullable();
+            $table->string('website')->length('255')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('perusahans');
+    }
+}
