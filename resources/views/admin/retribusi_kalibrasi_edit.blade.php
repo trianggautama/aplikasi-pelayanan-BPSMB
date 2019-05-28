e352c5027cf7b2004ae4525059acb829678b343e
Tom's <toms@Tomss-MacBook-Pro.local>
2019-05-28 10:40:10 +0800

    add method put retribusi kalibrasi edit

--- resources/views/admin/retribusi_kalibrasi_edit.blade.php
+++ resources/views/admin/retribusi_kalibrasi_edit.blade.php
@@ -5,33 +5,39 @@
     <!--input sizes starts-->
     <div class="col-lg-12">
         <div class="card" style="margin-top:20px;">
-            <div class="card-header"><h5 class="card-header-text">Input Sizes</h5>
+            <div class="card-header"><h5 class="card-header-text">Edit Data</h5>
                 <div class="f-right">
                     <a href="" data-toggle="modal" data-target="#input-size-Modal"><i class="icofont icofont-code-alt"></i></a>
                 </div>
             </div>
+            <form  method="post" action="" enctype="multipart/form-data">
+                {{method_field('PUT') }}
+                {{ csrf_field() }}
             <div class="card-block">
                 <div class="form-group row">
                     <div class="col-md-2"><label for="InputNormal" class="form-control-label">Nama</label></div>
-                    <div class="col-md-10"><input type="text" class="form-control" id="InputNormal"  placeholder="Nama Barang"></div>
+                <div class="col-md-10"><input type="text" name="nama" class="form-control" id="InputNormal"  placeholder="Nama Barang" value="{{ $Kalibrasi->nama }}"></div>
                 </div>
                 <div class="form-group row">
                     <div class="col-md-2"><label for="InputNormal" class="form-control-label">Rentang Ukur</label></div>
-                    <div class="col-md-10"><input type="text" class="form-control" id="InputNormal"  placeholder="Rentang Ukur"></div>
+                    <div class="col-md-10"><input type="text" name="rentang_ukur" class="form-control" id="InputNormal"  placeholder="Rentang Ukur" value="{{ $Kalibrasi->rentang_ukur }}"></div>
                 </div>
                 <div class="form-group row">
                     <div class="col-md-2"><label for="InputNormal" class="form-control-label">Biaya</label></div>
-                    <div class="col-md-10"><input type="text" class="form-control" id="InputNormal"  placeholder="Biaya"></div>
+                    <div class="col-md-10"><input type="text" name="biaya" class="form-control" id="InputNormal"  placeholder="Biaya" value="{{ $Kalibrasi->biaya }}"></div>
                 </div>
                 <div class="form-group row">
                     <div class="col-md-2"><label for="InputNormal" class="form-control-label">Keterangan</label></div>
-                    <div class="col-md-10"> <textarea name="" class="form-control" id="" cols="30" rows="10"></textarea></div>
+                    <div class="col-md-10"> <textarea name="keterangan" class="form-control" id="" cols="30" rows="10">{{ $Kalibrasi->keterangan }}</textarea></div>
                 </div>
+                {{-- {{ csrf_field() }} --}}
             </div>
             <div class="card-footer text-right">
-            <a href="{{route('retribusi_kalibrasi_index')}}" class="btn btn-inverse-danger">Batal</a>
-                <a href="" class="btn btn-inverse-primary">Ubah Data</a>
+                <a href="{{route('retribusi_kalibrasi_index')}}" class="btn btn-inverse-danger">Batal</a>
+                {{-- <button type="submit" class="btn btn-inverse-primary">Ubah Data</button> --}}
+                <button type="submit" name="submit" class="btn btn-inverse-primary">Ubah</button>
             </div>
+            {{-- </form> --}}
         </div>
     </div>
     <!--input sizes ends-->
