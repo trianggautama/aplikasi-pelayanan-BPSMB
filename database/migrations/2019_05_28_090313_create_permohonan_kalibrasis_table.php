03f24ccc17a83a758d16b40aaaf4e73cd1c49f67
Tom's <toms@Tomss-MacBook-Pro.local>
2019-05-29 11:13:34 +0800

    make migration permohonan kalibrasi

---
+++ database/migrations/2019_05_28_090313_create_permohonan_kalibrasis_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePermohonanKalibrasisTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('permohonan_kalibrasis', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('id_perusahaan');
+            $table->bigInteger('id_retribusi_kalibrasi');
+            $table->date('tanggal');
+            $table->string('merk')->length(100);
+            $table->string('no_seri')->length(100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('permohonan_kalibrasis');
+    }
+}
