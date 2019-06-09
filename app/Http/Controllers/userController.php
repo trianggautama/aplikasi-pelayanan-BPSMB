fd334d81555d68b383211de19a2aff07d7efed09
Tom's <toms@Tomss-MacBook-Pro.local>
2019-06-09 20:42:58 +0800

    add retribusi kalibrasi use

--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Perusahaan;
 use App\Permohonan_kalibrasi;
+use App\Retribusi_kalibrasi;
+use Carbon\Carbon;
 
 class userController extends Controller
 {
@@ -26,9 +28,32 @@ class userController extends Controller
 
         // tambah permohonan kalibarsi user
         public function permohonan_kalibrasi_tambah(){
-
-        return view('users.permohonan_kalibrasi_tambah');
+        $id = auth::id();    
+        $Kalibrasi  = Retribusi_kalibrasi::all();
+        $Perusahaan = Perusahaan::where('id_user',$id)->first();
+        $Date = Carbon::now();
+        return view('users.permohonan_kalibrasi_tambah',compact('Kalibrasi','Perusahaan','Date'));
          }
+
+        public function permohonan_kalibrasi_store(Request $request){
+
+        $Kalibrasi = new Permohonan_kalibrasi;
+        
+        // $Date = Carbon::now()->toDateString();
+
+        $Kalibrasi->id_user                 = $request->id_user;
+        $Kalibrasi->id_perusahaan           = $request->id_perusahaan;
+        $Kalibrasi->id_retribusi_kalibrasi  = $request->id_retribusi_kalibrasi;
+        // $Kalibrasi->tanggal                 = $Date;
+        $Kalibrasi->tanggal                    = $request->tanggal;
+        $Kalibrasi->merk                    = $request->merk;
+        $Kalibrasi->no_seri                 = $request->no_seri;
+    // dd($request);
+
+        $Kalibrasi->save();
+       
+          return redirect(route('permohonan_kalibrasi_user_index'))->with('success', 'Data permohonan kalibrasi '.$request->merk.' Berhasil di Tambahkan');
+      }//fungsi menambahkan data permohonan kalibrasi
 
 
       //edit permohonan kalibarsi user
