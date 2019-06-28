0b6ddf9272e7ad04ca653c0ea800843baa4ecc46
Toms <toms@Tomss-MacBook-Pro.local>
2019-06-29 06:50:34 +0800

    change function model to default foreign key lara

--- app/Permohonan_kalibrasi.php
+++ app/Permohonan_kalibrasi.php
@@ -7,10 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Permohonan_kalibrasi extends Model
 {
     public function perusahaan(){
-        return $this->belongsTo('App\Perusahaan','id_perusahaan');
+        return $this->belongsTo('App\Perusahaan','perusahaan_id');
       }
 
     public function retribusi(){
-        return $this->belongsTo('App\Retribusi_kalibrasi','id_retribusi_kalibrasi');
-      }  
+        return $this->belongsTo('App\Retribusi_kalibrasi','retribusi_kalibrasi_id');
+      }
 }
--- app/Perusahaan.php
+++ app/Perusahaan.php
@@ -9,6 +9,6 @@ class Perusahaan extends Model
     protected $table = 'perusahaans';
 
     public function user(){
-      return $this->belongsTo('App\User', 'id_user');
+      return $this->belongsTo('App\User', 'user_id');
     }
 }
