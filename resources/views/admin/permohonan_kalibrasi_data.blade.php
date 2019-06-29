e119918228e822aa190017eecac1aa6fee2bd9e4
tri angga utama <trianggategarutama@gmail.com>
2019-06-29 19:16:39 +0800

    perbaikan tampilan permohonan kalibrasi

--- resources/views/admin/permohonan_kalibrasi_data.blade.php
+++ resources/views/admin/permohonan_kalibrasi_data.blade.php
@@ -12,7 +12,7 @@
         <div class="card-header ">
                 <h4>Data Permohonan Kalibrasi</h4>
                 <div class="text-right">
-                        <a class="btn btn-inverse-success" href=""><i class="icon-arrow-add"></i>cetak data</a>
+                        <a class="btn btn-inverse-success" href=""><i class="icofont icofont-printer"></i> cetak data</a>
                     </div>
         </div>
         <div class="card-block">
@@ -26,31 +26,26 @@
                             <th>Barang Kalibrasi</th>
                             <th>Biaya</th>
                             <th>Tanggal</th>
-                            <th>Merk</th>
-                            <th>No Seri</th>
                             <th class="text-center">Action</th>
                         </tr>
                         </thead>
                         <tbody>
-                        <tr>
                             <?php $no = 0 ?>                                
                             @foreach ($Kalibrasi as $d) 
+                            <tr>
                             <td>{{$no = $no + 1}}</td>
                             <td>{{$d->perusahaan->user->name}}</td>
                             <td>{{$d->retribusi->nama}}</td>
-                            <td>{{$d->retribusi->biaya}}</td>
+                            <td>Rp. {{$d->retribusi->biaya}}</td>
                             <td>{{$d->tanggal}}</td>
-                            <td>{{$d->merk}}</td>
-                            <td>{{$d->no_seri}}</td>
                             <td class="text-center">
-                            {{-- <a href=" {{route('rambu-terpasang-detail', ['id' => IDCrypt::Encrypt( $d->id)])}}" class="btn btn-default"> <i class=" fa fa-eye"></i></a>
-                            <a href=" {{route('rambu-terpasang-edit', ['id' => IDCrypt::Encrypt( $d->id)])}}" class="btn btn-primary"> <i class=" far fa-edit"></i></a> --}}
-                            <a href="" class="btn btn-success">Verifikasi<i class=" "></i></a>
-                            <button type="button" class="btn btn-danger"
-                            onclick="Hapus('{{Crypt::encryptString($d->id)}}')"><b><i class="far fa-trash-alt"></i></b></button>
+                            <a href="" class="btn btn-inverse-primary" data-toggle="tooltip" data-placement="top" title="Detail"> <i class="icon-eye"></i></i></a>
+                            <a href="" class="btn btn-inverse-success" data-toggle="tooltip" data-placement="top" title="Verifikasi" ><i class="icon-check"></i></a>
+                            <button type="button" class="btn btn-inverse-danger" data-toggle="tooltip" data-placement="top" title="Hapus"
+                            onclick="Hapus('{{Crypt::encryptString($d->id)}}')"><b><i class="icon-trash"></i></b></button>
                         </td>
-                        @endforeach   
                         </tr>
+                        @endforeach   
                         </tbody>
                     </table>
                 </div>
@@ -59,44 +54,4 @@
     </div>
 </div>
 
-
-<!-- Modal -->
-<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered" role="document">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h5 class="modal-title" id="exampleModalLongTitle">Tambah Data</h5>
-              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-              </button>
-            </div>
-            <div class="modal-body">
-
-                        <div class="md-input-wrapper">
-                            <input type="text" class="md-form-control md-static" />
-                            <label>Nama</label>
-                        </div>
-                        <div class="md-input-wrapper">
-                            <input type="text" class="md-form-control md-static" />
-                            <label>Rentang Ukur</label>
-                        </div>
-                        <div class="md-input-wrapper">
-                                <input type="text" class="md-form-control md-static" />
-                                <label>Biaya</label>
-                            </div>
-                            <div class="md-input-wrapper">
-                                    <textarea class="md-form-control md-static" cols="2" rows="4"></textarea>
-                                    <label>Keterangan </label>
-                                </div>
-
-            </div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-inverse-danger" data-dismiss="modal">Close</button>
-              <button type="button" class="btn btn-inverse-primary">Save changes</button>
-            </form>
-            </div>
-          </div>
-        </div>
-      </div>
-    <!-- content-wrapper ends -->
 @endsection
