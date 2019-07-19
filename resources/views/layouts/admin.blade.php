35c5b200b6a4c6d6ba2f32dec7272936fd6344a2
Toms <toms@Tomss-MacBook-Pro.local>
2019-07-19 20:54:13 +0800

    fix admin photo layout

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -84,7 +84,7 @@
                         <!-- User Menu-->
                         <li class="dropdown">
                             <a href="#!" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false" class="dropdown-toggle drop icon-circle drop-image">
-                                <span><img class="img-circle " src="{{asset('assets/images/avatar-1.png')}}" style="width:40px;" alt="User Image"></span>
+                                <span><img class="img-circle " src="{{ asset('/images/admin/'.Auth::user()->foto) }}" style="width:40px;" alt="User Image"></span>
                                 <span>{{ Auth::user()->name }}<i class=" icofont icofont-simple-down"></i></span>
 
                             </a>
@@ -168,7 +168,7 @@
             <section class="sidebar" id="sidebar-scroll">
 
                 <div class="user-panel">
-                    <div class="f-left image"><img src="{{asset('assets/images/avatar-1.png')}} " alt="User Image" class="img-circle"></div>
+                    <div class="f-left image"><img src="{{ asset('/images/admin/'.Auth::user()->foto) }}" alt="User Image" class="img-circle"></div>
                     <div class="f-left info">
                         <p>{{ Auth::user()->name }}</p>
                         <p class="designation">Admin <i class="icofont icofont-caret-down m-l-5"></i></p>
