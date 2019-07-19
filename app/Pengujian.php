67982b82f64e55fcdccf085ded6dfa6111d4bf95
Toms <toms@Tomss-MacBook-Pro.local>
2019-07-20 00:27:58 +0800

    add function user

--- app/Pengujian.php
+++ app/Pengujian.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Pengujian extends Model
 {
+    public function user(){
+        return $this->belongsTo('App\User','user_id');
+      }
+
     public function permohonan_pengujian(){
         return $this->belongsTo('App\permohonan_pengujian','permohonan_pengujian_id');
       }
