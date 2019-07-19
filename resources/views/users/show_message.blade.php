302aa0f56b17daa35f2381d279ea6429e71c72df
Toms <toms@Tomss-MacBook-Pro.local>
2019-07-19 20:57:39 +0800

    add show message view user

--- resources/views/users/show_message.blade.php
+++ resources/views/users/show_message.blade.php
@@ -71,8 +71,9 @@
                                         <div>
                                             <div class="card email-card">
                                                 <div class="card-header">
-                                                    <h5 class="card-header-text text-capitalize d-inline-block">Here you have new opportunity...</h5>
-                                                    <h6 class="f-right">08:23 AM</h6>
+                                                    {{-- @if($inbox->permohonan_pengujian->status) --}}
+                                                    <h5 class="card-header-text text-capitalize d-inline-block">Hallo</h5>
+                                                    <h6 class="f-right">{{ $date->format('g:i A') }}</h6>
                                                 </div>
                                                 <div class="card-block">
                                                     <div class="media m-b-20">
@@ -83,13 +84,13 @@
                                                         </div>
                                                         <div class="media-body photo-contant">
 
-                                                                <a href="#"><h6 class="user-name txt-primary">John Doe</h6></a>
-                                                                <a class="user-mail txt-muted" href="#"><h6>From:johndoe1543@gmail.com</h6></a>
+                                                                <a href="#"><h6 class="user-name txt-primary">Admin</h6></a>
+                                                                <a class="user-mail txt-muted" href="#"><h6>From:admin@gmail.com</h6></a>
 
                                                             <div>
-                                                                <h6 class="email-welcome-txt">Hello Dear...</h6>
+                                                                <h6 class="email-welcome-txt">{{ $inbox->subjek }}</h6>
                                                                 <p class="email-content">
-                                                                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar,
+                                                                    {{ $inbox->keterangan }}
                                                                 </p>
                                                             </div>
                                                         </div>
