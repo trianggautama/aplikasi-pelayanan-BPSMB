fc81635bf6b1605f32c609ac8f0bac5b28f60af6
Toms <toms@Tomss-MacBook-Pro.local>
2019-07-19 20:50:18 +0800

    add permohonan kalibrasi data view

--- resources/views/admin/permohonan_kalibrasi_data.blade.php
+++ resources/views/admin/permohonan_kalibrasi_data.blade.php
@@ -7,7 +7,7 @@
 
         </div>
     </div>
- 
+
     <div class="card">
         <div class="card-header ">
                 <h4>Data Permohonan Kalibrasi</h4>
@@ -21,19 +21,47 @@
                     <table class="table table-hover" id="myTable">
                         <thead>
                         <tr>
-                            <th>No</th>
-                            <th>Nama Perusahaan</th>
-                            <th>Barang Kalibrasi</th>
-                            <th>Biaya</th>
-                            <th>Tanggal</th>
-                            <th class="text-center">Action</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                            <?php $no = 0 ?>                                
-                            @foreach ($Kalibrasi as $d) 
-                           
-                        @endforeach   
+                                <th>No</th>
+                                <th>Nama Perusahaan</th>
+                                <th>Barang Kalibrasi</th>
+                                <th>Biaya</th>
+                                <th>Tanggal</th>
+                                <th>Merk</th>
+                                <th>No Seri</th>
+                                <th>Status</th>
+                                <th class="text-center">Action</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                             <?php $no = 0 ?>
+                                @foreach ($Kalibrasi as $d)
+                                <td>{{$no = $no + 1}}</td>
+                                <td>{{$d->perusahaan->user->name}}</td>
+                                <td>{{$d->retribusi->nama}}</td>
+                                <td>{{$d->retribusi->biaya}}</td>
+                                <td>{{$d->tanggal}}</td>
+                                <td>{{$d->merk}}</td>
+                                <td>{{$d->no_seri}}</td>
+                                <td>
+                                @if($d->status == 0)
+                                <label class="label bg-danger">Ditolak</label>
+                                @elseif($d->status == 1)
+                                <label class="label bg-warning">Pending</label>
+                                @elseif($d->status == 2)
+                                <label class="label bg-info">Diterima</label>
+                                @endif
+                                </td>
+                                <td class="text-center">
+                                <a href="{{Route('halaman_verifikasi_kalibrasi', ['id' => IDCrypt::Encrypt( $d->id)])}}" class="btn btn-inverse-success" data-toggle="tooltip" data-placement="top" title="Verifikasi" ><i class="icon-check"></i></a>
+                                <a href="{{ route('permohonan_kalibrasi_edit', ['id' => IDCrypt::Encrypt( $d->id)])}}" class="btn btn-inverse-primary" data-toggle="tooltip" data-placement="top" title="Edit"><i class="icofont icofont-edit-alt"></i></a>
+                                <a href="{{ route('permohonan_kalibrasi_hapus', ['id' => IDCrypt::Encrypt( $d->id)])}}" class="btn btn-inverse-danger" data-toggle="tooltip" data-placement="top" title="hapus"><i class="icofont icofont-ui-delete"></i></a>
+                                {{-- <a href="{{ route('permohonan_kalibrasi_edit', ['id' => IDCrypt::Encrypt( $d->id)])}}" class="btn btn-inverse-primary" data-toggle="tooltip" data-placement="top" title="Detail"><i class="icofont icofont-edit-alt"></i></a> --}}
+                                {{-- <a href=" {{route('permohonan_kalibrasi_edit', ['id' => IDCrypt::Encrypt( $d->id)])}}" class="btn btn-primary"> <i class=" far fa-edit"></i></a> --}}
+                            </td>
+
+                            </tr>
+                            @endforeach
                         </tbody>
                     </table>
                 </div>
