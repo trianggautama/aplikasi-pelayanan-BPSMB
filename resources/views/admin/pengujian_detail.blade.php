76144fc9dd2e5ad50101dc0e9acdf9489fbdf076
Toms <toms@Tomss-MacBook-Pro.local>
2019-07-20 08:07:43 +0800

    show data pengujian detail view

--- resources/views/admin/pengujian_detail.blade.php
+++ resources/views/admin/pengujian_detail.blade.php
@@ -21,43 +21,61 @@
                                         <tbody>
                                         <tr>
                                             <th scope="row">Nama Perusahaan</th>
-                                            <td>CV. ABDI JAYA PLUS</td>
+                                            <td>{{ $pengujian->user->name }}</td>
                                         </tr>
                                         <tr>
                                             <th scope="row">Pengujian</th>
-                                            <td> Kalibrasi kadar abu</td>
+                                            <td> {{ $pengujian->permohonan_pengujian->retribusi->komoditi }}</td>
                                         </tr>
                                         <tr>
                                             <th scope="row">Biaya</th>
-                                            <td>Rp.2.500.000</td>
+                                            <td>Rp.{{ $pengujian->permohonan_pengujian->retribusi->biaya }},-</td>
                                         </tr>
                                         <tr>
                                             <th scope="row">Tanggal Verifikasi</th>
-                                            <td> 18 Juni 2019</td>
+                                            <td> {{ $pengujian->created_at->format('d M Y') }}</td>
                                         </tr>
                                         <tr>
                                             <th scope="row">Tanggal Pengujian</th>
-                                            <td>30 Juni 2019</td>
+                                            <td>{{ $pengujian->tanggal }}</td>
                                         </tr>
                                         <tr>
                                             <th scope="row">Estimasi</th>
-                                            <td>1 bulan</td>
+                                            <td>{{ $pengujian->estimasi }}</td>
                                         </tr>
                                         <tr>
                                             <th scope="row">keterangan uji</th>
-                                            <td><label class="label bg-warning">Dalam Proses Uji</label></td>
+                                            <td>
+                                                @if($pengujian->status == 0)
+                                                    <label class="label bg-danger">Gagal Uji</label>
+                                                        @elseif($pengujian->status == 2)
+                                                    <label class="label bg-warning">Pending</label>
+                                                        @elseif($pengujian->status == 1)
+                                                    <label class="label bg-info">Sedang Diuji</label>
+                                                        @elseif($pengujian->status == 3)
+                                                    <label class="label bg-success">Selesai Diuji</label>
+                                                    @endif
+                                            </td>
                                         </tr>
                                         <tr>
                                             <th scope="row">Metode Pembayaran</th>
-                                            <td><label class="label bg-primary">cash</label></td>
+                                            <td>
+                                                @if($pengujian->metode_pembayaran == 1)
+                                                    <label class="label bg-info">Cash</label>
+                                                    @elseif($pengujian->metode_pembayaran == 2)
+                                                    <label class="label bg-success">Transfer</label>
+                                                    @else
+                                                    <label class="label bg-warning">Belum Dibayar</label>
+                                                @endif
+                                            </td>
                                         </tr>
                                         <tr>
                                             <th scope="row">Lain-lain</th>
-                                            <td>-</td>
+                                            <td>{{ $pengujian->lainnya }}</td>
                                         </tr>
                                         <tr>
                                             <th scope="row">Keterangan</th>
-                                            <td>-</td>
+                                            <td>{{ $pengujian->keterangan }}</td>
                                         </tr>
                                         </tbody>
                                     </table>
@@ -74,10 +92,10 @@
         </div>
         <div class="card-footer text-right">
             <a href="" class="btn btn-danger"><i class="icofont icofont-ui-delete"></i> Hapus Data</a>
-            <a href="{{Route('pengujian_edit')}}" class="btn btn-info"><i class="icofont icofont-edit-alt"></i> Edit Data</a>
-            <a href="" class="btn btn-primary"> <i class="icofont icofont-printer"></i> Cetak tanda Terima</a>
-            <a href="{{Route('hasil_pengujian_tambah')}}" class="btn btn-success">Input Hasil Pengujian</a>
-            <a href="{{Route('sertifikat_pengujian')}}" class="btn btn-primary"> <i class="icofont icofont-printer"></i> Cetak Sertifikat</a>
+            <a href="{{Route('pengujian_edit',['id'=>IDCrypt::Encrypt($pengujian->id)])}}" class="btn btn-info"><i class="icofont icofont-edit-alt"></i> Edit Data</a>
+            <a href="" class="btn btn-primary"> <i class="icofont icofont-printer"></i> Cetak Detail Data</a>
+            <a href="{{Route('hasil_pengujian_tambah',['id'=>IDCrypt::Encrypt($pengujian->id)])}}" class="btn btn-success"> <i class="icofont "></i> input Hasil Pengujian</a>
+            <a href="{{Route('sertifikat_pengujian',['id'=>IDCrypt::Encrypt($pengujian->id)])}}" class="btn btn-primary"> <i class="icofont icofont-edit-alt"></i> Cetak Sertifikat</a>
 
         </div>
     </div>
