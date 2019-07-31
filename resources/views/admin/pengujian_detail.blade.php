48b456c9ed2eb64661173846c60131520534795f
muhammadasadbastomi <tomycules@gmail.com>
2019-07-31 13:25:49 +0800

    +ket uji,tgl terima barang&selesai,validate button

--- resources/views/admin/pengujian_detail.blade.php
+++ resources/views/admin/pengujian_detail.blade.php
@@ -32,17 +32,29 @@
                                             <td>Rp.{{ $pengujian->permohonan_pengujian->retribusi->biaya }},-</td>
                                         </tr>
                                         <tr>
+                                            <th scope="row">Keterangan Permohonan</th>
+                                            <td>{{ $pengujian->permohonan_pengujian->keterangan }}</td>
+                                        </tr>
+                                        <tr>
                                             <th scope="row">Tanggal Verifikasi</th>
-                                            <td> {{ $pengujian->created_at->format('d M Y') }}</td>
+                                            <td> {{ $pengujian->created_at->format('d-m-Y') }}</td>
                                         </tr>
                                         <tr>
                                             <th scope="row">Tanggal Antar Barang</th>
-                                            <td>{{ carbon\carbon::parse($pengujian->permohonan_pengujian->inbox->tanggal)->format('d M Y') }}</td>
+                                            <td>{{ carbon\carbon::parse($pengujian->permohonan_pengujian->inbox->tanggal)->format('d-m-Y') }}</td>
+                                        </tr>
+                                        <tr>
+                                            <th scope="row">Tanggal Terima Barang</th>
+                                            @if(isset($pengujian->tanggal_terima))
+                                            <td>{{ carbon\carbon::parse($pengujian->tanggal_terima)->format('d-m-Y') }}</td>
+                                            @else
+                                            <td></td>
+                                            @endif
                                         </tr>
                                         <tr>
                                             <th scope="row">Tanggal Pengujian</th>
                                             @if(isset($pengujian->tanggal))
-                                            <td>{{ carbon\carbon::parse($pengujian->tanggal)->format('d M Y') }}</td>
+                                            <td>{{ carbon\carbon::parse($pengujian->tanggal)->format('d-m-Y') }}</td>
                                             @else
                                             <td></td>
                                             @endif
@@ -51,9 +63,23 @@
                                             <th scope="row">Estimasi</th>
                                             <td>{{ $pengujian->estimasi }}</td>
                                         </tr>
+                                        @if($pengujian->status==3)
+                                        <tr>
+                                            <th scope="row">Tanggal Selesai</th>
+                                            @if(isset($pengujian->updated_at))
+                                            <td>{{ carbon\carbon::parse($pengujian->updated_at)->format('d-m-Y') }}</td>
+                                            @else
+                                            <td></td>
+                                            @endif
+                                        </tr>
+                                        @else
+                                        @endif
                                         <tr>
                                             <th scope="row">keterangan uji</th>
                                             <td>
+                                                @if(isset($pengujian->sertifikat))
+                                                <label class="label bg-success">Terverifikasi</label>
+                                                @else
                                                 @if($pengujian->status == 0)
                                                     <label class="label bg-danger">Gagal Uji</label>
                                                         @elseif($pengujian->status == 2)
@@ -62,6 +88,7 @@
                                                     <label class="label bg-info">Sedang Diuji</label>
                                                         @elseif($pengujian->status == 3)
                                                     <label class="label bg-success">Selesai Diuji</label>
+                                                    @endif
                                                     @endif
                                             </td>
                                         </tr>
@@ -100,15 +127,22 @@
         </div>
         <div class="card-footer text-right">
             <a href="{{ route('pengujian_hapus', ['id' => IDCrypt::Encrypt( $pengujian->id)])}}" class="btn btn-inverse-danger"><i class="icofont icofont-ui-delete"></i>Hapus Data</a>
+            @if($pengujian->status==3)
+            {{-- <a href="" class="btn btn-danger"><i class="icofont icofont-ui-delete"></i> Hapus Data</a> --}}
+            @else
             <a href="{{Route('pengujian_edit',['id'=>IDCrypt::Encrypt($pengujian->id)])}}" class="btn btn-info"><i class="icofont icofont-edit-alt"></i> Edit Data</a>
+            @endif
             @if(isset($pengujian->tanggal))
             <a href="{{Route('nota_permohonan_pengujian',['id'=>IDCrypt::Encrypt($pengujian->id)])}}" class="btn btn-primary"> <i class="icofont icofont-printer"></i> Cetak Tanda Terima</a>
-            <a href="{{Route('hasil_pengujian_tambah',['id'=>IDCrypt::Encrypt($pengujian->id)])}}" class="btn btn-success"> <i class="icofont "></i> input Hasil pengujian</a>
             @else
             @endif
             @if(isset($pengujian->hasil_pengujian->id))
-            <a href="{{Route('sertifikat_pengujian',['id'=>IDCrypt::Encrypt($pengujian->id)])}}" class="btn btn-primary"> <i class="icofont icofont-edit-alt"></i> Cetak Sertifikat</a>
-            <a href="{{Route('pengujian_sertifikat_edit',['id'=>IDCrypt::Encrypt($pengujian->id)])}}" class="btn btn-primary"> <i class="icofont icofont-edit-alt"></i> Upload Sertifikat</a>
+            @elseif($pengujian->status==3)
+            <a href="{{Route('hasil_pengujian_tambah',['id'=>IDCrypt::Encrypt($pengujian->id)])}}" class="btn btn-success"> <i class="icofont "></i> input Hasil Pengujian</a>
+            @endif
+            @if(isset($pengujian->hasil_pengujian->id))
+            <a href="{{Route('sertifikat_pengujian',['id'=>IDCrypt::Encrypt($pengujian->id)])}}" class="btn btn-primary"> <i class="icofont icofont-printer"></i> Cetak Sertifikat</a>
+            <a href="{{Route('pengujian_sertifikat_edit',['id'=>IDCrypt::Encrypt($pengujian->id)])}}" class="btn btn-primary"> <i class="icofont icofont-upload"></i> Upload Sertifikat</a>
             @else
             @endif
 
